588ac9bb01dd9b8655aa73ebd5c121d5a1f7f7e5
EllisDzen <ellisdzen@gmai.com>
2024-03-04 03:21:34 +0300

    upgrade test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,13 @@
 from utils import arrs
 
 
+# В функции test_get(): изменила ожидаемое значение в первом утверждении на 2, так как индексация начинается с 0, а не с 1.
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
+    assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
 
 
+# В функции test_slice(): изменений не потребовалось, так как тесты соответствуют ожиданиям.
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
